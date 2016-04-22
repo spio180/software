f998f9331bbabb601375fe8836e22b394464a913
Pawel Kos <spio180.pi@gmail.com>
2016-04-22 21:52:34 +0200

    Initial commit of server project

---
+++ iKomunikator_server/src/iKomunikator_server/Main.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package iKomunikator_server;
+
+/**
+ * @author pkos
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Server started");
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}

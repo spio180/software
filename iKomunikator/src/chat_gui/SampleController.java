2b7482bf1b5282ed27fa90843264ca434aece08f
pkosGit <pkos@op.pl>
2016-04-23 21:25:16 +0200

    Tests with Java FX

--- iKomunikator/src/chat_gui/SampleController.java
+++ iKomunikator/src/chat_gui/SampleController.java
@@ -1,5 +1,31 @@
 package chat_gui;
 
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+
 public class SampleController {
-	
+	@FXML Button butCancel;
+	@FXML Button butConnect;
+
+	private TcpClientConnection ConnectionToServer;
+
+	@FXML private void closeButtonAction(){
+	    // get a handle to the stage
+	    Stage stage = (Stage) butCancel.getScene().getWindow();
+	    // do what you have to do
+	    stage.close();
+	}
+
+	@FXML private void connectButtonAction() {
+		ConnectionToServer = new TcpClientConnection();
+		ConnectionToServer.TcpConnect();
+		
+		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+	    alert.setTitle("Message Here...");
+	    alert.setHeaderText("Look, an Information Dialog");
+	    alert.setContentText("I have a great message for you!");
+	    alert.showAndWait();
+	}
 }

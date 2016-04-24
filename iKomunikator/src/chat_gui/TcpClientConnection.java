53d91a8b56973bb9ba68f3b25506e36fd16330aa
spio15 <pkos@op.pl>
2016-04-24 13:11:42 +0200

    add comment to TcpClientConnection

--- iKomunikator/src/chat_gui/TcpClientConnection.java
+++ iKomunikator/src/chat_gui/TcpClientConnection.java
@@ -1,6 +1,10 @@
 package chat_gui;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class TcpClientConnection {
@@ -21,6 +25,20 @@ public class TcpClientConnection {
 	}
 	
 	public int Connect() throws UnknownHostException{
+		
+		/*
+		String sentence;
+		String modifiedSentence;
+		BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
+		Socket clientSocket = new Socket("localhost", 6789);
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		sentence = inFromUser.readLine();
+		outToServer.writeBytes(sentence + '\n');
+		modifiedSentence = inFromServer.readLine();
+		System.out.println("FROM SERVER: " + modifiedSentence);
+		clientSocket.close();
+		*/
 		return 0;
 		
 	}

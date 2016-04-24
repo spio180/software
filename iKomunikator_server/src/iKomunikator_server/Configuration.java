579c36712a7cb01fe7f30a2f16e6ee8cf2a43874
wozniakl <l.wozniak@tenesys.pl>
2016-04-24 11:58:46 +0200

    Added: Read server conf from file

---
+++ iKomunikator_server/src/iKomunikator_server/Configuration.java
@@ -0,0 +1,71 @@
+package iKomunikator_server;
+
+import org.apache.commons.configuration2.XMLConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+
+/**
+ * Created by lukasz on 24.04.16.
+ */
+public class Configuration {
+    private String mServerIp;
+    private int mPortNumber;
+    private int mConnectionTimeout;
+    private String mConfigFilePath = "server.cfg";
+
+    public void loadConfigFromFile(String path) {
+        if(path != null){
+            mConfigFilePath = path;
+        }
+
+        Parameters params = new Parameters();
+        FileBasedConfigurationBuilder<XMLConfiguration> builder =
+                new FileBasedConfigurationBuilder<XMLConfiguration>(XMLConfiguration.class)
+                        .configure(params.xml()
+                                .setFileName(mConfigFilePath));
+        XMLConfiguration readConfig = null;
+        try {
+            readConfig = builder.getConfiguration();
+            mServerIp = readConfig.getString("server_ip");
+            mPortNumber = readConfig.getInt("port_number");
+            mConnectionTimeout = readConfig.getInt("connection_timeout");
+        } catch (ConfigurationException e) {
+            System.out.printf("Error reading the config file\n");
+            if(Main.debug == true) e.printStackTrace();
+        }
+    }
+
+    public String getServerIp() {
+        return mServerIp;
+    }
+
+    public void setServerIp(String serverIp) {
+        mServerIp = serverIp;
+    }
+
+    public int getPortNumber() {
+        return mPortNumber;
+    }
+
+    public void setPortNumber(int portNumber) {
+        mPortNumber = portNumber;
+    }
+
+    public int getConnectionTimeout() {
+        return mConnectionTimeout;
+    }
+
+    public void setConnectionTimeout(int connectionTimeout) {
+        mConnectionTimeout = connectionTimeout;
+    }
+
+    public String getConfigFilePath() {
+        return mConfigFilePath;
+    }
+
+    public void setConfigFilePath(String configFilePath) {
+        mConfigFilePath = configFilePath;
+    }
+
+}
--- iKomunikator_server/src/iKomunikator_server/Main.java
+++ iKomunikator_server/src/iKomunikator_server/Main.java
@@ -9,15 +9,15 @@ package iKomunikator_server;
  */
 public class Main {
 
+	public static final Boolean debug = Boolean.TRUE;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Server started");
-		
-		
-		// TODO Auto-generated method stub
 
+		Configuration conf = new Configuration();
+		conf.loadConfigFromFile("iKomunikator_server/server.cfg");
+		System.out.printf("%s %d %d\n",conf.getServerIp(),conf.getPortNumber(),conf.getConnectionTimeout());
 	}
 
 }

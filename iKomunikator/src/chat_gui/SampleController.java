d27b4b817754fb0c91681a23f6077275bfa023d5
pkosGit <pkos@op.pl>
2016-05-28 10:20:51 +0200

    server test

--- iKomunikator/src/chat_gui/SampleController.java
+++ iKomunikator/src/chat_gui/SampleController.java
@@ -5,7 +5,6 @@ import java.net.UnknownHostException;
 
 import javafx.fxml.FXML;
 import javafx.stage.Stage;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
@@ -18,18 +17,18 @@ public class SampleController {
 	@FXML Pane tcpClientPort;
 	@FXML TextField textClientPort;
 	private TcpClientConnection ConnectionToServer;
-	
-	
+
+
 	@FXML private void closeButtonAction(){
 	    // get a handle to the stage
 	    Stage stage = (Stage) butCancel.getScene().getWindow();
 	    // do what you have to do
 	    stage.close();
 	}
-	 
+
 	@FXML private void connectButtonAction() {
 		ConnectionToServer = new TcpClientConnection();
-		
+
 		try {
 			ConnectionToServer.createClientSocket(textClientIP.getText(),
 					Integer.parseInt(textClientPort.getText()));
@@ -41,22 +40,10 @@ public class SampleController {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			//e.printStackTrace();
-		
+
 			System.out.println("Error during creating of Socket");
 		}
-		/*
-		try {
-			ConnectionToServer.Connect();
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		Alert alert = new Alert(Alert.AlertType.INFORMATION);
-	    alert.setTitle("Message Here...");
-	    alert.setHeaderText("Look, an Information Dialog");
-	    alert.setContentText("I have a great message for you!");
-	    alert.showAndWait();
-	    */
+
+
 	}
 }
--- iKomunikator/src/chat_gui/TcpClientConnection.java
+++ iKomunikator/src/chat_gui/TcpClientConnection.java
@@ -8,69 +8,81 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import javafx.scene.control.Alert;
+
 public class TcpClientConnection {
 	String ClientIP;
 	int ClientPort;
 	Socket ClientSocket;
-	
+
 	TcpClientConnection() {
 		ClientIP = "localhost";
 		ClientPort = 10001;
 		ClientSocket = null;
-		
+
 	}
-	
+
 	public static String getCurrentIPAddress() throws UnknownHostException{
-		
+
 		InetAddress localaddr = InetAddress.getLocalHost();
-		
+
 		System.out.println ("Local Address : " + localaddr );
 		System.out.println("Local IP: " + localaddr.getHostAddress());
 		System.out.println ("Local hostname   : " + localaddr.getHostName());
-		
+
 		return localaddr.getHostAddress();
-		
+
 	}
-	
+
 	public void createClientSocket(String IP, int Port) throws IOException{
-		
+
 		if (IP == null) {
 			ClientIP = getCurrentIPAddress();
 		}
 		else ClientIP = IP;
-		
+
 		System.out.println("Client IP: " + ClientIP);
-		
+
 		if (Port != ClientPort && Port != 0){
 			ClientPort = Port;
 		}
 		System.out.println("Client Port: " + ClientPort);
-		
+
 		ClientSocket = new Socket(ClientIP, ClientPort);
 	}
-	
-	
+
+
 	public void Connect(String clientLogin) {
-		
-		String message = "Hello Server - Client \n" + clientLogin;
+
+		String message = "Hello Server - Client: " + clientLogin + "\n";
 		System.out.println(message);
-		
+
 		if (ClientSocket != null) {
-		
+
 			DataOutputStream outToServer;
 			try {
 				outToServer = new DataOutputStream(ClientSocket.getOutputStream());
 				BufferedReader inFromServer = new BufferedReader(new InputStreamReader(ClientSocket.getInputStream()));
 				outToServer.writeBytes(message);
-		
+
 				String serverAnswear = inFromServer.readLine();
 				System.out.println("FROM SERVER: " + serverAnswear);
+
 				ClientSocket.close();
+
+				Alert alert = new Alert(Alert.AlertType.INFORMATION);
+			    alert.setTitle("Message from SERVER");
+			    alert.setHeaderText("Received message");
+			    alert.setContentText(serverAnswear);
+			    alert.showAndWait();
+
+
+
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 	}
-	
+
 }

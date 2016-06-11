1b9fb1b1e77643d07732d7318d6cd30fb71de2b2
krzysztof <krzysztof@10.10.10.139>
2016-06-11 09:46:46 +0200

    wsd

---
+++ common_code/src/common/Const.java
@@ -0,0 +1,11 @@
+package common;
+
+public class Const {
+	public static String MSG_LOGOWANIE = "LOGIN_REQ";
+	public static String MSG_LOGOWANIE_OK = "LOGIN_OK";
+	public static String MSG_LOGOWANIE_BLAD = "LOGIN_ERR";	
+	public static String MSG_WYLOGOWANIE = "LOGOUT";
+	public static String MSG_DO_WSZYSTKICH = "SEND_MSG_ALL";
+	public static String MSG_DO_UZYTKOWNIKA = "SEND_MSG_USER";
+	public static String MSG_LISTA_UZ = "USERS_LIST";	
+}
